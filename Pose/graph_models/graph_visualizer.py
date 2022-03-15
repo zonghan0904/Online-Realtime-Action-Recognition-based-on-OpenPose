7e0d892247dbf7a9aea77f4119de89465dca302e
zonghan0904 <steven.me05@nctu.edu.tw>
2022-03-15 17:27:36 +0800

    visualize each neural network model

---
+++ Pose/graph_models/graph_visualizer.py
@@ -0,0 +1,17 @@
+import tensorflow.compat.v1 as tf
+
+def graph_visualizer(graph_path):
+    with tf.gfile.GFile(graph_path, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+    graph = tf.get_default_graph()
+    tf.import_graph_def(graph_def, name='TfPoseEstimator')
+    sess = tf.Session(graph=graph)
+
+    with tf.Session() as sess:
+        writer = tf.summary.FileWriter("logs", sess.graph)
+        writer.close()
+
+if __name__ == "__main__":
+    graph_visualizer("./VGG_origin/graph_opt.pb")
